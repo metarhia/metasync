e058b43006f419a02020e30d79c81635b64f57b0
Vlad Dziuba <dzyubavlad@gmail.com>
2017-01-05 09:41:04 +0200

    Added isomorphism between callback and metacallback categories

--- metasync.js
+++ metasync.js
@@ -574,3 +574,13 @@ metasync.timeout = (timeout, asyncFunction, doneFunction) => {
     }
   });
 };
+
+// Wrap metacallback to common callback
+//    callback - function to wrap
+metasync.fromMetaCallback = callback => (err, data) => callback(err || data);
+
+// Wrap common callback to metacallback
+//    callback - function to wrap
+metasync.toMetaCallback   = callback => dataE => dataE instanceof Error
+                                               ? callback(dataE)
+                                               : callback(null, dataE);
