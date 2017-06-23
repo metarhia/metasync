1fe989b322b2b1fdae7eabda695afb8b58729619
Vlad Dziuba <dzyubavlad@gmail.com>
2017-06-23 13:01:24 +0300

    Change const syntax to function in flow tests
    Fixes: #143
    Reviewed-By: Timur Shemsedinov <timur.shemsedinov@gmail.com>
    PR-URL: https://github.com/metarhia/metasync/pull/157

--- test/flow.js
+++ test/flow.js
@@ -7,13 +7,13 @@ tap.test('flow with parallel flow', (test) => {
   const data = { test: 'data' };
   const expectedData = { test: 'data', fn1: 'data 1', fn2: 'data 2' };
 
-  const fn1 = (data, cb) => process.nextTick(() => {
-    cb(null, 'data 1');
-  });
+  function fn1(data, cb) {
+    process.nextTick(() => cb(null, 'data 1'));
+  }
 
-  const fn2 = (data, cb) => process.nextTick(() => {
-    cb(null, 'data 2');
-  });
+  function fn2(data, cb) {
+    process.nextTick(() => cb(null, 'data 2'));
+  }
 
   const fc = metasync.flow([[fn1, fn2 ]]);
   fc(data, (err, data) => {
@@ -26,13 +26,13 @@ tap.test('flow with parallel flow', (test) => {
 tap.test('parallel with error', (test) => {
   const parallelError = new Error('Parallel error');
 
-  const fn1 = (data, cb) => process.nextTick(() => {
-    cb(null, 'data 1');
-  });
+  function fn1(data, cb) {
+    process.nextTick(() => cb(null, 'data 1'));
+  }
 
-  const fn2 = (data, cb) => process.nextTick(() => {
-    cb(parallelError);
-  });
+  function fn2(data, cb) {
+    process.nextTick(() => cb(parallelError));
+  }
 
   metasync.parallel([fn1, fn2 ], (err, res) => {
     test.strictSame(err, parallelError);
@@ -45,14 +45,16 @@ tap.test('sequential with error', (test) => {
   const sequentialError = new Error('Sequential error');
   const expectedDataInFn2 = { fn1: 'data 1' };
 
-  const fn1 = (data, cb) => process.nextTick(() => {
-    cb(null, 'data 1');
-  });
+  function fn1(data, cb) {
+    process.nextTick(() => cb(null, 'data 1'));
+  }
 
-  const fn2 = (data, cb) => process.nextTick(() => {
-    tap.strictSame(data, expectedDataInFn2);
-    cb(sequentialError);
-  });
+  function fn2(data, cb) {
+    process.nextTick(() => {
+      tap.strictSame(data, expectedDataInFn2);
+      cb(sequentialError);
+    });
+  }
 
   metasync.sequential([fn1, fn2], (err, res) => {
     test.strictSame(err, sequentialError);
@@ -71,30 +73,40 @@ tap.test('flow with complex flow', (test) => {
     expectedDataInRes['fn' + i] = 'data ' + i;
   }
 
-  const fn1 = (data, cb) => process.nextTick(() => {
-    tap.strictSame(data, expectedDataInFn1);
-    cb(null, 'data 1');
-  });
+  function fn1(data, cb) {
+    process.nextTick(() => {
+      tap.strictSame(data, expectedDataInFn1);
+      cb(null, 'data 1');
+    });
+  }
 
-  const fn2 = (data, cb) => process.nextTick(() => {
-    test.strictSame(data, expectedDataInFn2);
-    cb(null, 'data 2');
-  });
+  function fn2(data, cb) {
+    process.nextTick(() => {
+      test.strictSame(data, expectedDataInFn2);
+      cb(null, 'data 2');
+    });
+  }
 
-  const fn3 = (cb) => process.nextTick(() => {
-    cb(null, 'data 3');
-  });
+  function fn3(cb) {
+    process.nextTick(() => {
+      cb(null, 'data 3');
+    });
+  }
 
-  const fn4 = (cb) => process.nextTick(() => {
-    cb(null, 'data 4');
-  });
+  function fn4(cb) {
+    process.nextTick(() => {
+      cb(null, 'data 4');
+    });
+  }
 
-  const fn5 = (data, cb) => process.nextTick(() => {
-    test.strictSame(data.fn1, 'data 1');
-    test.strictSame(data.fn2, 'data 2');
-    test.strictSame(data.fn4, 'data 4');
-    cb(null, 'data 5');
-  });
+  function fn5(data, cb) {
+    process.nextTick(() => {
+      test.strictSame(data.fn1, 'data 1');
+      test.strictSame(data.fn2, 'data 2');
+      test.strictSame(data.fn4, 'data 4');
+      cb(null, 'data 5');
+    });
+  }
 
   const fc = metasync.flow([fn1, fn2, [[fn3, [fn4, fn5] ]], [], [[ ]] ]);
   fc(data, (err, data) => {
