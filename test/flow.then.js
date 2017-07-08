45b5a5144d0eb4e2cf966dab8692102618dfde90
Vlad Dziuba <dzyubavlad@gmail.com>
2017-07-08 17:57:28 +0200

    Implement then method for flow
    Closes: #171
    Reviewed-By: Timur Shemsedinov <timur.shemsedinov@gmail.com>
    PR-URL: https://github.com/metarhia/metasync/pull/186

---
+++ test/flow.then.js
@@ -0,0 +1,54 @@
+'use strict';
+
+const tap = require('tap');
+const metasync = require('..');
+
+tap.test('successfull then', (test) => {
+  let finishedFuncsCount = 0;
+  const res1 = {};
+  function af1(data, callback) {
+    return process.nextTick(() => {
+      test.strictSame(data, {});
+      test.strictSame(finishedFuncsCount, 0);
+      finishedFuncsCount++;
+      callback(null, res1);
+    });
+  }
+  const res2 = {};
+  function af2(data, callback) {
+    return process.nextTick(() => {
+      test.strictSame(data, { af1: res1 });
+      test.strictSame(finishedFuncsCount, 1);
+      finishedFuncsCount++;
+      callback(null, res2);
+    });
+  }
+  const res3 = {};
+  function af3(data, callback) {
+    return process.nextTick(() => {
+      const keysCount = Object.keys(data).length;
+      test.ok(keysCount >= 2 && keysCount < 4);
+      test.ok(finishedFuncsCount >= 2 && finishedFuncsCount < 4);
+      finishedFuncsCount++;
+      callback(null, res3);
+    });
+  }
+  const res4 = {};
+  function af4(data, callback) {
+    return process.nextTick(() => {
+      const keysCount = Object.keys(data).length;
+      test.ok(keysCount >= 2 && keysCount < 4);
+      test.ok(finishedFuncsCount >= 2 && finishedFuncsCount < 4);
+      finishedFuncsCount++;
+      callback(null, res4);
+    });
+  }
+  const faf1 = metasync.flow([af1, af2]);
+  const faf2 = metasync.flow([[af3, af4]]);
+  faf1.then(faf2)({}, (err, res) => {
+    test.error(err);
+    test.strictSame(res, { af1: res1, af2: res2, af3: res3, af4: res4 });
+    test.strictSame(finishedFuncsCount, 4);
+    test.end();
+  });
+});
