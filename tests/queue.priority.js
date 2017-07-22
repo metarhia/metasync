df90d41b48b253fc8ecc7a1212643700e24e22e6
tshemsedinov <timur.shemsedinov@gmail.com>
2017-07-22 21:12:48 +0300

    Improve Queue
    - Rename .factor() to .roundRobin()
    - Allow .add(item, priority) roundRobin mode isn't active

--- tests/queue.priority.js
+++ tests/queue.priority.js
@@ -9,12 +9,12 @@ const q = metasync.queue(3)
     setTimeout(cb, 100);
   });
 
-q.add({ id: 1 }, 0,   0);
-q.add({ id: 2 }, 0,   0);
-q.add({ id: 3 }, 0,   1);
-q.add({ id: 4 }, 0,   0);
-q.add({ id: 5 }, 0,   0);
-q.add({ id: 6 }, 0,  10);
-q.add({ id: 7 }, 0,   0);
-q.add({ id: 8 }, 0, 100);
-q.add({ id: 9 }, 0,   0);
+q.add({ id: 1 },   0);
+q.add({ id: 2 },   0);
+q.add({ id: 3 },   1);
+q.add({ id: 4 },   0);
+q.add({ id: 5 },   0);
+q.add({ id: 6 },  10);
+q.add({ id: 7 },   0);
+q.add({ id: 8 }, 100);
+q.add({ id: 9 },   0);
