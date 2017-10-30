7f0f2e18c0044eef67db86b3c72929097de2b3a0
tshemsedinov <timur.shemsedinov@gmail.com>
2017-10-31 01:27:07 +0200

    Improve parameter/result passing between functions in flow
    Closes: https://github.com/metarhia/metasync/issues/243
    PR-URL: https://github.com/metarhia/metasync/pull/246

--- test/flow.then.js
+++ test/flow.then.js
@@ -5,7 +5,7 @@ const metasync = require('..');
 
 tap.test('successfull then', (test) => {
   let finishedFuncsCount = 0;
-  const res1 = {};
+  const res1 = 'res1';
   function af1(data, callback) {
     return process.nextTick(() => {
       test.strictSame(data, {});
@@ -14,7 +14,7 @@ tap.test('successfull then', (test) => {
       callback(null, res1);
     });
   }
-  const res2 = {};
+  const res2 = 'res2';
   function af2(data, callback) {
     return process.nextTick(() => {
       test.strictSame(data, { af1: res1 });
@@ -23,7 +23,7 @@ tap.test('successfull then', (test) => {
       callback(null, res2);
     });
   }
-  const res3 = {};
+  const res3 = 'res3';
   function af3(data, callback) {
     return process.nextTick(() => {
       const keysCount = Object.keys(data).length;
@@ -33,7 +33,7 @@ tap.test('successfull then', (test) => {
       callback(null, res3);
     });
   }
-  const res4 = {};
+  const res4 = 'res4';
   function af4(data, callback) {
     return process.nextTick(() => {
       const keysCount = Object.keys(data).length;
@@ -45,7 +45,12 @@ tap.test('successfull then', (test) => {
   }
   const faf1 = metasync([af1, [[af2, af3]], af4]);
   faf1.then((res) => {
-    test.strictSame(res, { af1: res1, af2: res2, af3: res3, af4: res4 });
+    test.strictSame(res, {
+      af1: 'res1',
+      af2: 'res2',
+      af3: 'res3',
+      af4: 'res4'
+    });
     test.strictSame(finishedFuncsCount, 4);
     test.end();
   }, (err) => {
