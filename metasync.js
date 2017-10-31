10f92a0716f2cd68ff345573068c1a16c605bf3e
tshemsedinov <timur.shemsedinov@gmail.com>
2017-10-31 17:11:03 +0200

    Move firstOf out of flow.js
    Closes: https://github.com/metarhia/metasync/issues/244
    PR-URL: https://github.com/metarhia/metasync/pull/251

--- metasync.js
+++ metasync.js
@@ -1,14 +1,15 @@
 'use strict';
 
 const submodules = [
-  'flow', // Flow control
+  'flow', // Control flow
+  'utilities', // Miscellaneous utilities
+  'fp', // Async utils for functional programming
+  'adapters', // Adapters to convert different async contracts
+  'throttle', // Throttling utilities
   'array', // Array utilities
   'chain', // Process arrays sync and async array in chain
   'collector', // DataCollector and KeyCollector
   'queue', // Concurrency
-  'throttle', // Throttling
-  'fp', // Async utils for functional programming
-  'adapters', // adapters to convert different async contracts
 ].map(path => require('./lib/' + path));
 
 const flow = submodules[0].flow;
