1ee71b3356639b36d094cd10ddb4cdf2146b054f
tshemsedinov <timur.shemsedinov@gmail.com>
2017-11-03 06:48:14 +0200

    Deprecate key naming after a function
    Closes: https://github.com/metarhia/metasync/issues/256
    PR-URL: https://github.com/metarhia/metasync/pull/260

--- test/flow.clone.js
+++ test/flow.clone.js
@@ -5,14 +5,18 @@ const metasync = require('..');
 
 tap.test('flow clone', (test) => {
   const data = { test: 'data' };
-  const expectedData = { test: 'data', fn1: 'data 1', fn2: 'data 2' };
+  const expectedData = { test: 'data', data1: 'data 1', data2: 'data 2' };
 
   function fn1(data, cb) {
-    process.nextTick(() => cb(null, 'data 1'));
+    process.nextTick(() => {
+      cb(null, { data1: 'data 1' });
+    });
   }
 
   function fn2(data, cb) {
-    process.nextTick(() => cb(null, 'data 2'));
+    process.nextTick(() => {
+      cb(null, { data2: 'data 2' });
+    });
   }
 
   const fc1 = metasync([[fn1, fn2]]);
@@ -21,8 +25,6 @@ tap.test('flow clone', (test) => {
   fc1(data, (err, data) => {
     test.error(err);
     test.strictSame(data, expectedData);
-    data.fn1 = '';
-    data.fn2 = '';
     fc2(data, (err, data) => {
       test.error(err);
       test.strictSame(data, expectedData);
