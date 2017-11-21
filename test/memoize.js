a49b1d01ead98f91b7237b3d24e482efed5746c0
tshemsedinov <timur.shemsedinov@gmail.com>
2017-11-21 04:09:56 +0200

    Async memoize first implementation
    Refs: https://github.com/metarhia/metasync/issues/268

---
+++ test/memoize.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const tap = require('tap');
+const metasync = require('..');
+
+tap.test('memoize', (test) => {
+  const storage = {
+    file1: Buffer.from('file1'),
+    file2: Buffer.from('file2'),
+  };
+
+  const getData = (file, callback) => {
+    process.nextTick(() => {
+      const result = storage[file];
+      if (result) callback(null, result);
+      else callback(new Error('File not found'));
+    });
+  };
+
+  const memoizedGetData = metasync.memoize(getData);
+
+  memoizedGetData('file1', (err, data) => {
+    test.error(err);
+    test.strictSame(data, storage.file1);
+    memoizedGetData('file2', (err, data) => {
+      test.error(err);
+      test.strictSame(data, storage.file2);
+      memoizedGetData('file1', (err, data) => {
+        test.error(err);
+        test.strictSame(data, storage.file1);
+        memoizedGetData('file2', (err, data) => {
+          test.error(err);
+          test.strictSame(data, storage.file2);
+          test.end();
+        });
+      });
+    });
+  });
+});
