9eab93feb077a73e2519b350cb8d78027512a661
tshemsedinov <timur.shemsedinov@gmail.com>
2017-12-05 22:28:24 +0200

    Add memoize.clear test
    PR-URL: https://github.com/metarhia/metasync/pull/273

--- test/memoize.js
+++ test/memoize.js
@@ -37,3 +37,31 @@ tap.test('memoize', (test) => {
     });
   });
 });
+
+tap.test('memoize clear cache', (test) => {
+  const storage = {
+    file1: Buffer.from('file1'),
+  };
+
+  const getData = (file, callback) => {
+    process.nextTick(() => {
+      const result = storage[file];
+      if (result) callback(null, result);
+      else callback(new Error('File not found'));
+    });
+  };
+
+  const memoizedGetData = metasync.memoize(getData);
+
+  memoizedGetData('file1', (err, data) => {
+    test.error(err);
+    test.strictSame(data, storage.file1);
+    storage.file1 = Buffer.from('changed');
+    memoizedGetData.clear();
+    memoizedGetData('file1', (err, data) => {
+      test.error(err);
+      test.strictSame(data, Buffer.from('changed'));
+      test.end();
+    });
+  });
+});
