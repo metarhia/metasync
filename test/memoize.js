4e2fdabf656bc33f1ad12f2217f9614c6003a247
tshemsedinov <timur.shemsedinov@gmail.com>
2018-01-01 23:43:27 +0200

    fixup! Add memoize functor methods

--- test/memoize.js
+++ test/memoize.js
@@ -65,3 +65,69 @@ tap.test('memoize clear cache', (test) => {
     });
   });
 });
+
+tap.test('memoize cache del', (test) => {
+  const storage = {
+    file1: Buffer.from('file1'),
+  };
+
+  const getData = (file, callback) => {
+    process.nextTick(() => {
+      const result = storage[file];
+      if (result) callback(null, result);
+      else callback(new Error('File not found'));
+    });
+  };
+
+  const memoizedGetData = metasync.memoize(getData);
+
+  memoizedGetData('file1', (err, data) => {
+    test.error(err);
+    test.strictSame(data, storage.file1);
+    storage.file1 = Buffer.from('changed');
+    memoizedGetData.del('file1');
+    memoizedGetData('file1', (err, data) => {
+      test.error(err);
+      test.strictSame(data, Buffer.from('changed'));
+      test.end();
+    });
+  });
+});
+
+tap.test('memoize cache add', (test) => {
+  const getData = (file, callback) => {
+    process.nextTick(() => {
+      const result = Buffer.from('added');
+      callback(null, result);
+    });
+  };
+
+  const memoizedGetData = metasync.memoize(getData);
+
+  const file1 = Buffer.from('added');
+  memoizedGetData.add('file1', null, file1);
+  memoizedGetData('file1', (err, data) => {
+    test.error(err);
+    test.strictSame(data, Buffer.from('added'));
+    test.end();
+  });
+});
+
+tap.test('memoize cache get', (test) => {
+  const getData = (file, callback) => {
+    process.nextTick(() => {
+      const result = Buffer.from('added');
+      callback(null, result);
+    });
+  };
+
+  const memoizedGetData = metasync.memoize(getData);
+
+  const file1 = Buffer.from('added');
+  memoizedGetData.add('file1', null, file1);
+  memoizedGetData.get('file1', (err, data) => {
+    test.error(err);
+    test.strictSame(data, Buffer.from('added'));
+    test.end();
+  });
+});
