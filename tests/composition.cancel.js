dc7728f4c7e520e5eb9dc8ae1d4737202887832d
tshemsedinov <timur.shemsedinov@gmail.com>
2018-06-23 14:03:16 +0300

    Async composition cancellation test

---
+++ tests/composition.cancel.js
@@ -0,0 +1,48 @@
+'use strict';
+
+const assert = require('assert');
+const metasync = require('..');
+
+const wrapAsync = (
+  // Emulate Asynchronous calls
+  callback // function
+) => {
+  setTimeout(callback, Math.floor((Math.random() * 500)));
+};
+
+let fc = null;
+
+const fn1 = (data, cb) => {
+  wrapAsync(() => {
+    assert.ok('fn1');
+    cb(null, 1);
+  });
+};
+
+const fn2 = (data, cb) => {
+  wrapAsync(() => {
+    assert.ok('fn2');
+    //if (fc) fc.cancel();
+    cb(null, 2);
+  });
+};
+
+const fn3 = (data, cb) => {
+  wrapAsync(() => {
+    //assert.fail('fn3');
+    cb(null, 3);
+  });
+};
+
+const fn4 = (data, cb) => {
+  wrapAsync(() => {
+    //assert.fail('fn4');
+    cb(null, 4);
+  });
+};
+
+fc = metasync([fn1, fn2, fn3, fn4]);
+
+fc([], (err, data) => {
+  assert.fail('done');
+});
