de6159368e277a41d6ae11c88ea459eb42e9772d
Dmytro Nechai <nechaido@gmail.com>
2018-08-07 18:59:44 +0300

    fixup! Add build for browser

--- prebuild.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-const fs = require('fs');
-
-const main = fs.readFileSync('./metasync.js').toString();
-fs.writeFileSync('./metasync.browser.js', main.replace('./lib/', './dist/'));
